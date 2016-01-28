350cd7cea49a0fb878f7dff72b6e60b755854883
Code Apprentice <codeguru@users.sourceforge.net>
2016-01-28 13:29:45 -0700

    `take` function

--- python/guruaes.py
+++ python/guruaes.py
@@ -3,3 +3,11 @@ def repeat(f, x):
   while True:
     yield x
     x = f(x)
+
+def take(n, seq):
+  l = []
+  for i, x in enumerate(seq):
+    if i == n:
+      return l
+    l.append(x)
+  return l
